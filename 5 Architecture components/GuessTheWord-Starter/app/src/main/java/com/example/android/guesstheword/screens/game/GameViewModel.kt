5c459189f1ac46bddb747861d4e9debbfd959e51
Roman Pelikh <pelikh.roman@gmail.com>
2020-02-23 12:42:46 -0400

    ex 5.4 done

--- 5 Architecture components/GuessTheWord-Starter/app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
+++ 5 Architecture components/GuessTheWord-Starter/app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
@@ -1,10 +1,9 @@
 package com.example.android.guesstheword.screens.game
 
+import android.os.CountDownTimer
+import android.text.format.DateUtils
 import android.util.Log
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.*
 
 
 //INHERIT FROM ViewModel
@@ -18,15 +17,39 @@ class GameViewModel : ViewModel() {
 
     // The current score
     private val _score = MutableLiveData<Int>();
-
     val score: LiveData<Int>
         get() = _score;
 
 
     private val _eventGameFinished = MutableLiveData<Boolean>();
-
     val eventGameFinished: LiveData<Boolean>
         get() = _eventGameFinished;
+
+
+    //Add Observable for current time
+    private val _currentTime = MutableLiveData<Long>();
+    val currentTime: LiveData<Long>
+        get() = _currentTime;
+
+
+    //MAP ELAPSED TIME AND FORMAT IT
+    //Allows us to convert LiveData object to another one
+    val currentTimeString = Transformations.map(currentTime) { time ->
+        DateUtils.formatElapsedTime(time);
+    }
+
+    //ADD HIN
+    val wordHint = Transformations.map(word) { word ->
+        val randomPosition = (1..word.length).random()
+        "Current word has " + word.length + " letters" +
+                "\nThe letter at position " + randomPosition + " is " +
+                word.get(randomPosition - 1).toUpperCase()
+    }
+
+
+    //Add variable for timer
+    private val timer: CountDownTimer
+
 
     // The list of words - the front of the list is the next word to guess
     lateinit var wordList: MutableList<String>
@@ -60,7 +83,39 @@ class GameViewModel : ViewModel() {
     }
 
 
+    companion object {
+        // Time when the game is over
+        private const val DONE = 0L
+
+        // Countdown time interval
+        private const val ONE_SECOND = 1000L
+
+        // Total time for the game
+        private const val COUNTDOWN_TIME = 60000L
+    }
+
+
     init {
+
+
+        //Init timer
+
+        //@param1 - TOTAL TIME
+        //@param2 - TIME INTERVAL
+        timer = object : CountDownTimer(COUNTDOWN_TIME, ONE_SECOND) {
+            //@param - the amount of time until the timer is finished in milliseconds
+            override fun onTick(millisUntilFinished: Long) {
+                _currentTime.value = millisUntilFinished / ONE_SECOND;
+            }
+
+            override fun onFinish() {
+                _currentTime.value = DONE;
+                onGameFinished();
+            }
+        }
+
+        //Start timer
+        timer.start();
 
         //Initialize default values for mutable live data
         _word.value = "";
@@ -73,16 +128,19 @@ class GameViewModel : ViewModel() {
     }
 
 
+    //Unsubscribe from everything
     override fun onCleared() {
         super.onCleared()
-        Log.i("GameViewModel", "GameViewModel destroyed")
+
+        //Cancel timer
+        timer.cancel();
+
     }
 
 
     fun nextWord() {
         if (wordList.isEmpty()) {
-
-            onGameFinished();
+            resetList();
         } else {
             //Select and remove a word from the list
             _word.value = wordList.removeAt(0)
